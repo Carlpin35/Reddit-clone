02fee76c3b1eb3f889271a26bbbb883ed9d74916
Carlpin35 <carlpin800@gmail.com>
2022-07-26 19:13:14 -0300

    26

--- src/components/Posts/comments/Comments.tsx
+++ src/components/Posts/comments/Comments.tsx
@@ -54,7 +54,7 @@ const Comments: React.FC<CommentsProps> = ({
 
 			const newComment: Comment = {
 				id: commentDocRef.id,
-				creatorId: user?.uid,
+				creatorId: user.uid,
 				creatorDisplayText: user.email!.split("@")[0],
 				communityId,
 				postId: selectedPost?.id!,
